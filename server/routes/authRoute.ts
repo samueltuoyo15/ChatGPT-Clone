e8ef71e7d70dacf7e842e1816bbb391f188eb363
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-04-24 16:21:19 +0100

    patch: deleted entire backend code i wana rewrite it again cuz the codes are tok old

--- server/routes/authRoute.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {registerUser, loginUser} from "../controllers/authController";
-
-const router = Router();
-
-router.post("/signUp", registerUser);
-router.post("/signIn", loginUser);
-
-export default router;
